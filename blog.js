9423b179949138ee2cd3374f6ec779185c1359ea
Shahnoor Mughal <122821502+shahnoor2003@users.noreply.github.com>
2023-07-14 02:34:20 -0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,21 @@
+ /*function dark(){
+    document.querySelector('.light').classList.toggle('dark');
+    document.querySelector('#pic').classList.toggle('pic');
+    document.querySelector('#pic2').classList.toggle('pic');
+   document.querySelector('body').classList.toggle('bg-dark');
+    const hr = document.querySelectorAll('#hr');
+      // Convert NodeList to an array
+      const hrr = Array.from(hr);
+     hrr.forEach((hr) => {
+      hr.classList.toggle('hr');
+    });
+    const item = document.querySelectorAll('a,h4,p,h3');
+      // Convert NodeList to an array
+      const everytext = Array.from(item);
+     everytext.forEach((item) => {
+      item.classList.toggle('text-white');
+    });
+}*/
+function Submit(){
+  alert("Thanks for your feedback!");
+}
